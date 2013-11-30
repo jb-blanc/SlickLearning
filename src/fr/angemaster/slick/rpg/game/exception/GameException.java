3daa2110d4f7c5fa57224619e168832aff89bb90
Jean-Baptiste BLANC <jboumboum@gmail.com>
2013-11-30 18:55:44 +0100

    Adding automatic hiding for exception popup

--- src/fr/angemaster/slick/rpg/game/exception/GameException.java
+++ src/fr/angemaster/slick/rpg/game/exception/GameException.java
@@ -5,25 +5,30 @@ import org.newdawn.slick.Graphics;
 
 public class GameException extends Exception{
 
+    private long startDisplay;
+
     public GameException(String message){
         super(message);
+        this.startDisplay = System.currentTimeMillis();
     }
 
     public void render(Graphics g){
-        g.setFont(GUIConstants.Exceptions.FONT);
-        float w = g.getFont().getWidth(this.getMessage());
-        float h = g.getFont().getHeight(this.getMessage());
-        float worldWidth = GUIConstants.WIDTH;
+        if(startDisplay + GUIConstants.Exceptions.HINT_DURATION > System.currentTimeMillis()){
+            g.setFont(GUIConstants.Exceptions.FONT);
+            float w = g.getFont().getWidth(this.getMessage());
+            float h = g.getFont().getHeight(this.getMessage());
+            float worldWidth = GUIConstants.WIDTH;
 
-        g.setColor(GUIConstants.Exceptions.BACKGROUND);
-        g.fillRect(
-                (worldWidth/2) - (w/2) - GUIConstants.Exceptions.PADDING,
-                GUIConstants.Exceptions.TOP,
-                w+(2*GUIConstants.Exceptions.PADDING),
-                h+(2*GUIConstants.Exceptions.PADDING)
-        );
-        g.setColor(GUIConstants.Exceptions.FONT_COLOR);
-        g.drawString(this.getMessage(), (worldWidth/2) - (w/2), GUIConstants.Exceptions.TOP+GUIConstants.Exceptions.PADDING);
+            g.setColor(GUIConstants.Exceptions.BACKGROUND);
+            g.fillRect(
+                    (worldWidth/2) - (w/2) - GUIConstants.Exceptions.PADDING,
+                    GUIConstants.Exceptions.TOP,
+                    w+(2*GUIConstants.Exceptions.PADDING),
+                    h+(2*GUIConstants.Exceptions.PADDING)
+            );
+            g.setColor(GUIConstants.Exceptions.FONT_COLOR);
+            g.drawString(this.getMessage(), (worldWidth/2) - (w/2), GUIConstants.Exceptions.TOP+GUIConstants.Exceptions.PADDING);
+        }
     }
 
 }
